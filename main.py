866a2a0476e7c2ed256a51f17c7be55acadd72c3
Purnima <pverma@uvic.ca>
2026-05-21 22:21:17 -0700

    Intial changes

---
+++ main.py
@@ -0,0 +1,11 @@
+from database import create_tables, get_connection
+from crypto import (
+    hash_master_password,
+    verify_master_password,
+    generate_key,
+    encrypt_password,
+    decrypt_password
+)
+#Both of thes
+
+def 
